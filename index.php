4bbf9e7e55266f5bfd760166f5b244af21444a75
MortuusBlasphemer <wagutierrez0707@gmail.com>
2024-06-17 15:36:16 -0400

    Comments

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php require 'App/Core/Autoload.php';
+<?php 
+
+//Archivo principal donde se cargaran las vistas y librerias correspondientes
+
+#carga del autoload
+require 'App/Core/Autoload.php';
 /*$conn = new AuthController;
 $conn->Index();*/
 
